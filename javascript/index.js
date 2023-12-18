d5febd9e6330afb21ff8abac11be7f911bd4cc3b
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-12-18 16:09:43 +0530

    smooth scroll done

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,11 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet");
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+// const scroll = new LocomotiveScroll();
 
 var tl = gsap.timeline()
 
@@ -23,3 +25,43 @@ tl.to("#home", {
     scale: 1,
     duration: 0.7
 })
+
+// Initialize Locomotive Scroll
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// Attach smooth scroll to navigation links with data-scroll-to attribute
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('[data-scroll-to]').forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            var targetId = this.getAttribute('href');
+
+            if (targetId.charAt(0) === '#') {
+                e.preventDefault();
+                smoothScroll(targetId);
+            }
+        });
+    });
+
+    function smoothScroll(targetId) {
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            gsap.to(window, {
+                duration: 1,
+                scrollTo: {
+                    y: targetElement,
+                    offsetY: 50, // Adjust this offset based on your layout
+                },
+                onComplete: () => {
+                    scroll.update(); // Update Locomotive Scroll after smooth scroll
+                },
+            });
+        }
+    }
+});
+
+
+
